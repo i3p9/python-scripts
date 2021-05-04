b928e9437183b3f2f0b9afb8abe2c5b208256de4
i3p9 <i3p9@users.noreply.github.com>
2021-05-04 07:36:49 +0600

    update to tesla, added argrs, prepping for 2fa

--- TeslaChargeReminder.py
+++ TeslaChargeReminder.py
@@ -1,23 +1,37 @@
+import argparse
 import teslapy
 import requests
 from twilio.rest import Client
 
-def checkStatus():
-    email = "something@me.com"
-    password = "hunter2"
+def parse():
+    parser = argparse.ArgumentParser(description='Tesla Charge Reminder via Twilio')
+    requredParser =parser.add_argument_group('Required arguments')
+
+    requredParser.add_argument('-e', '--email', help='Email for authentication.',required=True)
+    requredParser.add_argument('-p', '--password', help='Password for authentication.',required=True)
+    parser.add_argument('-o', '--otp', help='OTP for 2FA (OPTIONAL).', default='')
+
+    args = parser.parse_args()
+
+    email = args.email
+    password = args.password
+    otp = args.otp
+    return email, password, otp
+
+def checkStatus(email,password,otp):
     acceptedCharge = 75
 
-    with teslapy.Tesla(email, password) as tesla: # add lambda:'passcode' if mfa is enabled
+    with teslapy.Tesla(email, password) as tesla: # add lambda:'passcode' if mfa is enabled, it's not yet supported
         tesla.fetch_token()
         vehicles = tesla.vehicle_list()
         vehicles[0].sync_wake_up()
         chargePercentage = vehicles[0].get_vehicle_data()['charge_state']['battery_level']
         if (vehicles[0].get_vehicle_data()['charge_state']['charging_state'] != "Charging"):
             if(chargePercentage > acceptedCharge):
-                txt = "Car not plugged in and has "+chargePercentage+"% Charge! Plug it in"
+                txt = "Car not plugged in and has "+str(chargePercentage)+"% Charge! Plug it in"
                 sendTextMessage(txt)
         else:
-            txt = "Car is plugged in and has "+chargePercentage+"% Charge."
+            txt = "Car is plugged in and has "+str(chargePercentage)+"% Charge."
             sendTextMessage(txt)
 
 
@@ -28,4 +42,5 @@ def sendTextMessage(txt):
     client.messages.create(to=yourNumber, from_="+FromNumber", body=txt)
 
 if __name__ == '__main__':
-    checkStatus()
+    email, password,otp = parse()
+    checkStatus(email, password,otp)
